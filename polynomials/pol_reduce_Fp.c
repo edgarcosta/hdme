f9930afcaed1a2d4060f969abcec60c583f20ae3
Jean <jean.kieffer@math.cnrs.fr>
2022-06-07 17:26:16 -0400

    Fix verbose again

--- polynomials/pol_reduce_Fp.c
+++ polynomials/pol_reduce_Fp.c
@@ -14,8 +14,6 @@ int pol_reduce_Fp(fmpz_mod_poly_t red, const fmpz_poly_t num,
   
   fmpz_one(one);
   res = fmpz_mod_divides(invden, one, den, ctx);
-  
-  if (v && !res) flint_printf("(pol_reduce_Fp) Warning: denominator vanishes\n");
 
   if (res)
     {
