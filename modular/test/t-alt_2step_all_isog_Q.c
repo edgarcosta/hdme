f674891c96830941371143a1f2d1dbb7f0f037a7
Jean <jean.kieffer@math.cnrs.fr>
2022-06-07 15:11:38 -0400

    Test alt_2step_all_isog_Q with ell=7

--- modular/test/t-alt_2step_all_isog_Q.c
+++ modular/test/t-alt_2step_all_isog_Q.c
@@ -9,7 +9,8 @@ int main()
   flint_printf("alt_2step_all_isog_Q....");
   fflush(stdout);
 
-  for (iter = 0; iter < 1; iter++)
+  /* Up to iter < 4 */
+  for (iter = 0; iter < 2; iter++)
     {
       fmpz* I1;
       fmpz* I2;
@@ -38,7 +39,7 @@ int main()
 	  fmpz_set_si(&I2[2], 65765571);
 	  fmpz_set_si(&I2[3], 860544);
 	}
-      else
+      else if (iter == 1)
 	{
 	  /* https://beta.lmfdb.org/Genus2Curve/Q/277/a/277/1,
 	     https://beta.lmfdb.org/Genus2Curve/Q/277/a/277/2 */
@@ -51,6 +52,34 @@ int main()
 	  fmpz_set_si(&I2[1], 1370512);
 	  fmpz_set_si(&I2[2], 1511819744);
 	  fmpz_set_si(&I2[3], -1108);
+	}
+      else if (iter == 2)
+	{
+	  /* https://beta.lmfdb.org/Genus2Curve/Q/523/a/523/1,
+	     https://beta.lmfdb.org/Genus2Curve/Q/523/a/523/2 */	  
+	  fmpz_set_si(&I1[0], 120);
+	  fmpz_set_si(&I1[1], -540);
+	  fmpz_set_si(&I1[2], -29169);
+	  fmpz_set_si(&I1[3], -2092);
+	  ell = 5;
+	  fmpz_set_si(&I2[0], 332400);
+	  fmpz_set_si(&I2[1], 10084860);
+	  fmpz_set_si(&I2[2], 1107044456391);
+	  fmpz_set_si(&I2[3], -2092);
+	}
+      else
+	{
+	  /* https://beta.lmfdb.org/Genus2Curve/Q/295/a/295/1,
+	     https://beta.lmfdb.org/Genus2Curve/Q/295/a/295/2 */	  
+	  fmpz_set_si(&I1[0], 108);
+	  fmpz_set_si(&I1[1], -39);
+	  fmpz_set_si(&I1[2], 20835);
+	  fmpz_set_si(&I1[3], 37760);
+	  ell = 7;
+	  fmpz_set_si(&I2[0], 198804);
+	  fmpz_set_si(&I2[1], 305807001);
+	  fmpz_set_si(&I2[2], 18482629056189);
+	  fmpz_set_si(&I2[3], -37760);
 	}
       
       igusa_from_IC_fmpz(I1, I1);
