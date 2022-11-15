ac569d70798103b5d9a1c76d4388e50ab85d0a53
Edgar Costa <edgarc@mit.edu>
2022-11-15 16:53:01 -0500

    parallelizing hecke_collect_*

--- hecke/hecke_collect_siegel.c
+++ hecke/hecke_collect_siegel.c
@@ -1,30 +1,34 @@
 
 #include "hecke.h"
 
+/* See also hecke_collect_T1 */
+
 int hecke_collect_siegel(hecke_t H, slong ell, slong prec)
-{  
+{
   slong k;
   fmpz_mat_t gamma;
   slong nb = hecke_nb(H);
   int res = 1;
   int v = get_hecke_verbose();
-  
+
   fmpz_mat_init(gamma, 4, 4);
-  
+
   hecke_ell(H) = ell;
   hecke_check_nb(H, siegel_nb_cosets(ell));
-  
+
   if (v) hecke_collect_verbose_start(nb);
 
   /* Loop over all cosets to compute desired data */
-  for (k = 0; k < nb; k++)
-    {
-      if (v) hecke_collect_print_status(res, k, nb);
-      if (!res) break;
-      
-      siegel_coset(gamma, k, ell);
-      res = hecke_set_entry(H, k, gamma, prec);
-    }     
+  #pragma omp parallel for shared(res, H)
+  for (k = 0; k < nb; k++) {
+    if (v) hecke_collect_print_status(res, k, nb);
+    if (!res) continue; // OpenMP doesn't allow break, and thus we continue
+
+    siegel_coset(gamma, k, ell);
+    int localres = hecke_set_entry(H, k, gamma, prec);
+    #pragma omp atomic
+    res &= localres;
+  }
   if (v) flint_printf("\n");
 
   /* Set normalization factor */
@@ -32,7 +36,7 @@ int hecke_collect_siegel(hecke_t H, slong ell, slong prec)
   fmpz_set_si(hecke_norm_all(H), ell);
   fmpz_pow_ui(hecke_norm_all(H), hecke_norm_all(H), 2*hecke_nb(H) - (ell*ell + ell + 2));
   hecke_prod_ec(H) = n_pow(ell+1, 2);
-  
+
   fmpz_mat_clear(gamma);
-  return res;  
+  return res;
 }
