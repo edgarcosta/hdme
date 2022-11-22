4e9c82e27dda01d0722b46306dd8f051d16689f3
Edgar Costa <edgarc@mit.edu>
2022-11-22 09:39:39 -0500

    include progress bar and report

--- hecke/hecke_collect_T1.c
+++ hecke/hecke_collect_T1.c
@@ -4,9 +4,11 @@
 /* See also hecke_collect_siegel */
 
 int hecke_collect_T1(hecke_t H, slong ell, slong prec) {
+  time_pair start; timestamp_mark(&start);
   slong nb = hecke_nb(H);
   int res = 1;
   int v = get_hecke_verbose();
+  slong steps_completed = 0;
 
   hecke_ell(H) = ell;
   hecke_check_nb(H, siegel_nb_T1_cosets(ell));
@@ -15,28 +17,40 @@ int hecke_collect_T1(hecke_t H, slong ell, slong prec) {
 
 #pragma omp parallel
   {
+    int master = 0;
+#pragma omp single
+    {
+      master = 1;
+    }
     fmpz_mat_t gamma;
     fmpz_mat_init(gamma, 4, 4);
     /* Loop over all cosets to compute desired data */
 #pragma omp for schedule(static)
     for(slong k = 0; k < nb; ++k) {
-      if (v) hecke_collect_print_status(res, k, nb);
       if (!res) continue; // OpenMP doesn't allow break, and thus we continue
+      if (master && v) progress_bar(steps_completed, nb, "(hecke_collect)");
 
       siegel_T1_coset(gamma, k, ell);
       int localres = hecke_set_entry(H, k, gamma, prec);
 #pragma omp atomic
       res &= localres;
+#pragma omp atomic
+      ++steps_completed;
     }
     fmpz_mat_clear(gamma);
+    if (master && v) {
+      progress_bar(steps_completed, nb, "(hecke_collect)");
+      if (!res)
+        flint_printf("(hecke_collect) Warning: computation aborted due to low precision\n");
+    }
   }
-  if (v) flint_printf("\n");
 
+  /* Set normalization factor */
   hecke_norm_ind(H) = n_pow(ell, 3);
   fmpz_set_si(hecke_norm_all(H), ell);
-  fmpz_pow_ui(hecke_norm_all(H), hecke_norm_all(H),
-      3*hecke_nb(H) - (ell*ell + 2*ell + 1));
+  fmpz_pow_ui(hecke_norm_all(H), hecke_norm_all(H), 3*hecke_nb(H) - (ell*ell + 2*ell + 1));
   hecke_prod_ec(H) = 2*(ell*ell + ell);
 
+  if (v) report_end(start);
   return res;
 }
