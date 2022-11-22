ad79f3bad3dcd02c6c51a95a4cab6126c5a14e77
Edgar Costa <edgarc@mit.edu>
2022-11-22 14:17:04 -0500

    parallelize hecke_all_isog_Q

--- hecke/hecke_all_isog_Q.c
+++ hecke/hecke_all_isog_Q.c
@@ -1,76 +1,92 @@
 
 #include "hecke.h"
 
-int hecke_all_isog_Q(slong* nb_roots, fmpz* all_I, hecke_t H, fmpz* I, slong prec)
-{
-  slong k, j, i;
+int hecke_all_isog_Q(slong* nb_roots, fmpz* all_I, hecke_t H, fmpz* I, slong prec) {
+  time_pair start; timestamp_mark(&start);
   slong highprec;
   hecke_t H2;
-  fmpz* round;
-  fmpz_t zero;
-  acb_t m, c;
-  arf_t rad;
   int res;
   int v = get_hecke_verbose();
   slong weights[4] = IGUSA_HALFWEIGHTS;
+  slong nb = hecke_nb(H);
 
-  hecke_init(H2, hecke_nb(H));
-  round = _fmpz_vec_init(4);
-  fmpz_init(zero);
-  acb_init(m);
-  acb_init(c);
-  arf_init(rad);
-  
+  hecke_init(H2, nb);
+
   highprec = hecke_integral_highprec(H, prec);
   if (v) flint_printf("(hecke_all_isog_Q) Chosen high precision: %wd\n", highprec);
-  
+
   res = hecke_set_I_fmpz_with_lowprec(H2, I, hecke_theta2_tau(H), highprec);
   *nb_roots = 0;
-  
-  for (k = 0; k < hecke_nb(H); k++)
-    {
-      if (!res) break;
+
+  #pragma omp parallel
+	{ // this initializes the private	variables
+    fmpz* round;
+    fmpz_t zero;
+    acb_t m, c;
+    arf_t rad;
+
+    round = _fmpz_vec_init(4);
+    fmpz_init(zero);
+    acb_init(m);
+    acb_init(c);
+    arf_init(rad);
+
+    // filter out entries that need to be recomputed
+    #pragma omp for schedule(dynamic)
+    for (slong k = 0; k < nb; ++k) {
+      if (!res) continue; // OpenMP doesn't allow break, and thus we continue
       if (!acb_vec_contains_int(hecke_I_norm(H, k), 4)) continue;
 
       /* Round to nearest integers; if failure, abort with res = 0 */
-      for (j = 0; j < 4; j++)
-	{
-	  res = res && acb_round(&round[j], rad, &hecke_I_norm(H, k)[j]);
-	}
-      if (!res) break;
+      int localres = 1;
+      for (slong j = 0; j < 4; ++j) {
+        localres &= acb_round(&round[j], rad, &hecke_I_norm(H, k)[j]);
+      }
+#pragma omp atomic
+      res &=localres;
+      if (!res) continue; // OpenMP doesn't allow break, and thus we continue
 
       /* Recompute k-th entry of H at high precision */
-
-      if (v) flint_printf("(hecke_all_isog_Q) Recomputing entry at precision %wd\n", highprec);
+      if (v) flint_printf("(hecke_all_isog_Q) Recomputing %wd-th entry of H at high precision\n", k);
+      time_pair mid; timestamp_mark(&mid);
       hecke_set_entry(H2, k, hecke_coset(H, k), highprec);
+      if (v) report("hecke_set_entry", mid);
+
+      timestamp_mark(&mid);
       hecke_normalize_entry(H2, k, I, hecke_norm_ind(H), highprec);
+      if (v) report("hecke_normalize_entry", mid);
 
       /* Check norms are zero at low precision */
-      for (j = 0; j < 4; j++)
-	{
-	  acb_sub_fmpz(m, &hecke_I_norm(H2, k)[j], &round[j], highprec);
-	  for (i = 0; i < hecke_nb(H); i++)
-	    {
-	      if (i == k) continue;
-	      acb_sub_fmpz(c, &hecke_I_norm(H, i)[j], &round[j], prec);
-	      acb_mul(m, m, c, prec);
-	    }
-	  res = res && acb_round(zero, rad, m);
-	  res = res && fmpz_is_zero(zero);
-	}      
-      if (!res) break;
-      
-      /* Set result */
+      for (slong j = 0; j < 4; ++j) {
+        acb_sub_fmpz(m, &hecke_I_norm(H2, k)[j], &round[j], highprec);
+        for (slong i = 0; i < nb; ++i) {
+          if (i == k) continue;
+          acb_sub_fmpz(c, &hecke_I_norm(H, i)[j], &round[j], prec);
+          acb_mul(m, m, c, prec);
+        }
+        localres &= acb_round(zero, rad, m);
+        localres &= fmpz_is_zero(zero);
+      }
+      #pragma omp atomic
+      res &=localres;
+      if (!res) continue; // OpenMP doesn't allow break, and thus we continue
+
       cov_normalize_fmpz(round, round, 4, weights);
-      _fmpz_vec_set(&all_I[4*(*nb_roots)], round, 4);
-      *nb_roots += 1;
+      #pragma omp critical
+      {
+        /* Set result */
+        _fmpz_vec_set(&all_I[4*(*nb_roots)], round, 4);
+        *nb_roots += 1;
+      }
     }
-  
+    _fmpz_vec_clear(round, 4);
+    fmpz_clear(zero);
+    acb_clear(m);
+    acb_clear(c);
+    arf_clear(rad);
+  }
+
   hecke_clear(H2);
-  _fmpz_vec_clear(round, 4);
-  fmpz_clear(zero);
-  acb_clear(m);
-  acb_clear(c);
-  arf_clear(rad);
+  if (v) report_end(start);
   return res;
 }
