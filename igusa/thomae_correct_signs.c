51aa8f61732d5c9d057f2ba3a89740ea4878d370
Edgar Costa <edgarc@mit.edu>
2022-11-22 09:38:20 -0500

    cleaning up and adding some comments regarding openmp

--- igusa/thomae_correct_signs.c
+++ igusa/thomae_correct_signs.c
@@ -4,8 +4,8 @@
 
 
 int thomae_correct_signs(slong* perm, slong* signs, acb_srcptr roots,
-		acb_srcptr I, slong prec)
-{
+		acb_srcptr I, slong prec) {
+  time_pair start; timestamp_mark(&start);
 	// p in {0..719} s in {0..15}
 	slong nb_candidates = 720 * 16;
 	slong weights[4] = IGUSA_HALFWEIGHTS;
@@ -14,18 +14,12 @@ int thomae_correct_signs(slong* perm, slong* signs, acb_srcptr roots,
 	int res = 1;
 	int v = get_thomae_verbose();
 
-	int removed_ps[720][16];
-	int removed_p[720];
+	int removed_ps[720][16] = { 0 }; // all elements 0
+	int removed_p[720] = { 0 }; // all elements 0
 
 
-// initialize removed_p
-#pragma omp parallel for schedule(static)
-	for(slong p = 0; p < 720; p++) {
-		removed_p[p] = 0;
-		for (slong s = 0; s < 16; s++) removed_ps[p][s] = 0;
-	}
 
-#pragma omp parallel
+	#pragma omp parallel
 	{ // this initializes the private	variables
 		acb_ptr new_roots;
 		acb_ptr ros;
@@ -41,25 +35,25 @@ int thomae_correct_signs(slong* perm, slong* signs, acb_srcptr roots,
 		slong current_prec = thomae_startprec(prec);
 
 
-
-#pragma omp barrier
+		// put a barrier around the while guard
+		#pragma omp barrier
 		while((nb_candidates > 1) && (current_prec < prec)) {
-#pragma omp barrier
-#pragma omp single
+			#pragma omp barrier
+			#pragma omp single
 			{
 				if(v) flint_printf("(thomae_correct_signs) Trying precision %wd\n", current_prec);
 				nb_candidates = 0;
 			}
-#pragma omp barrier
-#pragma omp for schedule(static)
-			for (slong p = 0; p < 720; p++) {
+			#pragma omp barrier
+			#pragma omp for schedule(static)
+			for (slong p = 0; p < 720; ++p) {
 				if (removed_p[p]) continue;
 				removed_p[p] = 1;
 				thomae_reorder(new_roots, roots, p);
 				thomae_rosenhain(ros, new_roots, current_prec);
 				thomae_theta4(th4, ros, current_prec);
-				for(slong s = 0; s < 16; s++) {
-					if( removed_ps[p][s] ) continue;
+				for (slong s = 0; s < 16; ++s) {
+					if ( removed_ps[p][s] ) continue;
 					removed_ps[p][s] = 1;
 					thomae_theta2(th2, th4, ros, s, current_prec);
 					if (!thomae_discard(th2, current_prec)) {
@@ -71,16 +65,14 @@ int thomae_correct_signs(slong* perm, slong* signs, acb_srcptr roots,
 						int tau_success = theta2_inverse(tau, th2, current_prec);
 						/* We have to keep them in case of failure to compute. */
 						if (!tau_success || thomae_keep_candidate(tau, I, current_prec)) {
-#pragma omp atomic write
+							#pragma omp atomic write
 							correct_perm = p;
-#pragma omp atomic write
+							#pragma omp atomic write
 							correct_signs = s;
 							removed_p[p] = 0;
 							removed_ps[p][s] = 0;
-#pragma omp atomic update
+							#pragma omp atomic update
 							++nb_candidates;
-							/* acb_mat_printd(tau, 10);
-								 flint_printf("Succeeded computing tau: %d\n", tau_success); */
 						}
 						/* Else, tau was discarded: do nothing */
 					}
@@ -89,32 +81,36 @@ int thomae_correct_signs(slong* perm, slong* signs, acb_srcptr roots,
 				/* End for(s) loop */
 			}
 			/* End for(p) loop */
-#pragma omp single
+			// there is an implicit barrier at the end of the for loop
+			#pragma omp single
 			{
 				if(v) flint_printf("(thomae_correct_signs) Remaining candidates: %wd\n", nb_candidates);
 			}
 			current_prec *= THOMAE_MULPREC;
-#pragma omp barrier
+			// put a barrier around the while guard
+			#pragma omp barrier
 		}
 
-#pragma omp barrier
+		// put a barrier around the if guard
+		#pragma omp barrier
 		/* Then we have a last run at prec: this time we want to succeed in computing tau */
 		if (nb_candidates > 1) {
-#pragma omp barrier
-#pragma omp single
+		  #pragma omp barrier
+
+		  #pragma omp single
 			{
 				if(v) flint_printf("(thomae_correct_signs) Last run at precision %wd\n", prec);
 				nb_candidates = 0;
 			}
-#pragma omp barrier
+		  #pragma omp barrier
 
-#pragma omp for schedule(static)
-			for(slong p = 0; p < 720; p++) {
+		  #pragma omp for schedule(static)
+			for (slong p = 0; p < 720; ++p) {
 				if(nb_candidates) continue;
 				thomae_reorder(new_roots, roots, p);
 				thomae_rosenhain(ros, new_roots, prec);
 				thomae_theta4(th4, ros, prec);
-				for (slong s = 0; s < 16; s++) {
+				for (slong s = 0; s < 16; ++s) {
 					if(nb_candidates) continue;
 					// initialize
 					thomae_theta2(th2, th4, ros, s, current_prec);
@@ -125,7 +121,7 @@ int thomae_correct_signs(slong* perm, slong* signs, acb_srcptr roots,
 						if(tau_success)
 							tau_success = !cov_distinct(I_test, I, 4, weights, prec);
 						if(tau_success) {
-#pragma omp critical
+							#pragma omp critical
 							{
 								if(!nb_candidates) {
 									nb_candidates = 1;
@@ -157,5 +153,6 @@ int thomae_correct_signs(slong* perm, slong* signs, acb_srcptr roots,
 		*perm = correct_perm;
 		*signs = correct_signs;
 	}
+  if (v) report_end(start);
 	return res;
 }
