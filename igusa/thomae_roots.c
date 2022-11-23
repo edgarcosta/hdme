a23fa1c9cb9f031bdc298aab9b56e6b5fa06c797
Edgar Costa <edgarc@mit.edu>
2022-11-23 13:52:41 -0500

    use set_thomae_verbose

--- igusa/thomae_roots.c
+++ igusa/thomae_roots.c
@@ -13,13 +13,14 @@ int thomae_roots(acb_ptr roots, const acb_poly_t crv, slong prec)
   int res;
   n = acb_poly_find_roots(roots, crv, NULL, prec, prec);
   res = (n == acb_poly_degree(crv));
-  if (!res)
-    {
+  if (!res) {
+    if (get_thomae_verbose()) {
       flint_printf("(thomae_roots) Warning: unable to compute roots\n");
       flint_printf("(thomae_roots) Curve equation:\n");
       acb_poly_printd(crv, 10);
       flint_printf("\n(thomae_roots) Number of isolated roots: %wd\n", n);
       flint_printf("(thomae_roots) Working precision: %wd\n", prec);
     }
+  }
   return res;
 }
