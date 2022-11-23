72eda67d457c0edbb440dd0e6722726b48c56d25
Edgar Costa <edgarc@mit.edu>
2022-11-23 08:52:18 -0500

    30 log(ell)

--- modular/siegel_direct_isog_startprec.c
+++ modular/siegel_direct_isog_startprec.c
@@ -5,9 +5,9 @@ slong siegel_direct_isog_startprec(fmpz* I, slong ell)
 {
     slong weights[4] = IGUSA_WEIGHTS;
     slong h = cov_height(I, 4, weights);
-    
-    slong res = 10*h + 2*n_clog(ell,2) + 90;
-	
+
+    slong res = 10*h + 30*n_clog(ell,2) + 90;
+
     return 100 * (res/100 + 1);
 
 }
