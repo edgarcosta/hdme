f240e3891d3ff7e03c1f23cc5610eb99c4ff0bff
Edgar Costa <edgarc@mit.edu>
2022-11-23 08:51:51 -0500

    verbosity

--- modular/siegel_2step_direct_isog_Q.c
+++ modular/siegel_2step_direct_isog_Q.c
@@ -3,15 +3,18 @@
 
 int siegel_2step_direct_isog_Q(slong* nb, fmpz* all_I, fmpz* I, slong ell) {
   time_pair start; timestamp_mark(&start);
+
   slong prec = siegel_direct_isog_startprec(I, ell);
   hecke_t H;
   int stop = 0;
   int res = 1;
+  int v = get_modeq_verbose();
   *nb = 0; /* we might never call hecke_all_isog_Q */
 
   hecke_init(H, siegel_nb_T1_cosets(ell));
 
   while (!stop) {
+    if (v) flint_printf("(siegel_2step_direct_isog_Q) trying with prec = %wd\n", prec);
     res = hecke_set_I_fmpz(H, I, prec);
 
     if (res) res = hecke_collect_T1(H, ell, prec);
@@ -22,8 +25,9 @@ int siegel_2step_direct_isog_Q(slong* nb, fmpz* all_I, fmpz* I, slong ell) {
     prec = modeq_nextprec_generic(prec);
     stop = modeq_stop(res, prec);
   }
+  if (v) flint_printf("(siegel_2step_direct_isog_Q) succeeded with prec = %wd\n", prec);
 
   hecke_clear(H);
-  if (get_hecke_verbose()) report_end(start);
+  if (v) report_end(start);
   return res;
 }
