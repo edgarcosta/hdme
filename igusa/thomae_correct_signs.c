27f68bc332029dad24703a4beb133e1e12396af9
Edgar Costa <edgarc@mit.edu>
2022-11-27 22:52:42 -0500

    checking removed_p*

--- igusa/thomae_correct_signs.c
+++ igusa/thomae_correct_signs.c
@@ -56,25 +56,18 @@ int thomae_correct_signs(slong* perm, slong* signs, acb_srcptr roots,
 					if ( removed_ps[p][s] ) continue;
 					removed_ps[p][s] = 1;
 					thomae_theta2(th2, th4, ros, s, current_prec);
-					if (!thomae_discard(th2, current_prec)) {
-						/* flint_printf("Keeping (p,s) = (%wd,%wd)\n", p, s);
-							 for (k = 0; k < 4; k++)
-							 {
-							 acb_printd(&th2[k], 10); flint_printf("\n");
-							 } */
-						int tau_success = theta2_inverse(tau, th2, current_prec);
-						/* We have to keep them in case of failure to compute. */
-						if (!tau_success || thomae_keep_candidate(tau, I, current_prec)) {
-							#pragma omp atomic write
-							correct_perm = p;
-							#pragma omp atomic write
-							correct_signs = s;
-							removed_p[p] = 0;
-							removed_ps[p][s] = 0;
-							#pragma omp atomic update
-							++nb_candidates;
-						}
-						/* Else, tau was discarded: do nothing */
+					if (thomae_discard(th2, current_prec)) continue;
+					int tau_success = theta2_inverse(tau, th2, current_prec);
+					/* We have to keep them in case of failure to compute. */
+					if (!tau_success || thomae_keep_candidate(tau, I, current_prec)) {
+						#pragma omp atomic write
+						correct_perm = p;
+						#pragma omp atomic write
+						correct_signs = s;
+						removed_p[p] = 0;
+						removed_ps[p][s] = 0;
+						#pragma omp atomic update
+						++nb_candidates;
 					}
 					/* Else, th2 was discarded: do nothing */
 				}
@@ -107,33 +100,33 @@ int thomae_correct_signs(slong* perm, slong* signs, acb_srcptr roots,
 		  #pragma omp for schedule(static)
 			for (slong p = 0; p < 720; ++p) {
 				if(nb_candidates) continue;
+				if(removed_p[p]) continue;
 				thomae_reorder(new_roots, roots, p);
 				thomae_rosenhain(ros, new_roots, prec);
 				thomae_theta4(th4, ros, prec);
 				for (slong s = 0; s < 16; ++s) {
 					if(nb_candidates) continue;
+					if(removed_ps[p][s]) continue;
 					// initialize
 					thomae_theta2(th2, th4, ros, s, current_prec);
-					if(!thomae_discard(th2, current_prec)) {
-						int tau_success = theta2_inverse(tau, th2, current_prec);
-						if(tau_success)
-							tau_success = igusa_from_tau(I_test, tau, prec);
-						if(tau_success)
-							tau_success = !cov_distinct(I_test, I, 4, weights, prec);
-						if(tau_success) {
-							#pragma omp critical
-							{
-								if(!nb_candidates) {
-									nb_candidates = 1;
-									correct_perm = p;
-									correct_signs = s;
+					if(thomae_discard(th2, current_prec)) continue;
+					int tau_success = theta2_inverse(tau, th2, current_prec);
+					if (!tau_success) continue;
+					tau_success = igusa_from_tau(I_test, tau, prec);
+					if (!tau_success) continue;
+					tau_success = !cov_distinct(I_test, I, 4, weights, prec);
+					if (!tau_success) continue;
+					#pragma omp critical
+					{
+						if(!nb_candidates) {
+							nb_candidates = 1;
+							correct_perm = p;
+							correct_signs = s;
 
-									*perm = correct_perm;
-									*signs = correct_signs;
-									/* acb_mat_printd(tau, 10); */
-									if(v) flint_printf("(thomae_correct_signs) Found one!\n");
-								}
-							}
+							*perm = correct_perm;
+							*signs = correct_signs;
+							/* acb_mat_printd(tau, 10); */
+							if(v) flint_printf("(thomae_correct_signs) Found one! p=%wd s=%wd\n", p, s);
 						}
 					}
 				}
@@ -153,6 +146,6 @@ int thomae_correct_signs(slong* perm, slong* signs, acb_srcptr roots,
 		*perm = correct_perm;
 		*signs = correct_signs;
 	}
-  if (v) report_end(start);
+	if (v) report_end(start);
 	return res;
 }
